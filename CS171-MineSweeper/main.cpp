2231dc8f8765b5d51ff2065e0b1505db4595dbe6
ricardo <ricardoluliwei@gmail.com>
2020-04-30 23:09:13 -0700

    Initial Commit

---
+++ CS171-MineSweeper/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS171-MineSweeper
+//
+//  Created by Ricardo on 4/30/20.
+//  Copyright © 2020 Ricardo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
